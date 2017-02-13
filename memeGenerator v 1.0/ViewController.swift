82d3c1950ce117ff819c1c1892fbad30d16ccac2
Daniel McAteer <Danny@daniels-mbp-3.home>
2017-02-12 19:17:47 -0500

    Initial Commit

---
+++ memeGenerator v 1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  memeGenerator v 1.0
+//
+//  Created by Daniel McAteer on 2/12/17.
+//  Copyright © 2017 Daniel McAteer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
